d599e5c70fc5a81fa84d7548f2cd491aea500ed7
Laura Gourdel <laura.gourdel@gmail.com>
2018-05-10 17:55:18 +0200

    tenth

--- routes/categories.js
+++ routes/categories.js
@@ -19,5 +19,16 @@ router.get('/sorties', function(req, res, next) {
 router.get('/abuse', function(req, res, next) {
   res.render('abuse', { title: 'Signaler un abus' });
 });
-
+router.get('/cuisine', function(req, res, next) {
+  res.render('art', { title: 'Cuisine' });
+});
+router.get('/fete', function(req, res, next) {
+  res.render('fete', { title: 'Fête' });
+});
+router.get('/maxi', function(req, res, next) {
+  res.render('sorties', { title: 'Maxi bons plans' });
+});
+router.get('/divers', function(req, res, next) {
+  res.render('divers', { title: 'Divers' });
+});
 module.exports = router;
